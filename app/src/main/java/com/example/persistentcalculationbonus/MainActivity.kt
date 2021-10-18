67e48b93b03d5d603a72428b27110edaee31319b
RAM111111 <r.r.99@windowslive.com>
2021-10-18 13:32:57 +0300

    Save & restore fun

--- app/src/main/java/com/example/persistentcalculationbonus/MainActivity.kt
+++ app/src/main/java/com/example/persistentcalculationbonus/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.persistentcalculationbonus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var et2:EditText
     lateinit var btn:Button
 
+    var aa = 0.0
+    var bb =0.0f
+    var cc = 0.0f
+    var key = "myResult"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,27 +26,44 @@ class MainActivity : AppCompatActivity() {
         et1 = findViewById(R.id.editTextTextPersonName)
         et2 = findViewById(R.id.editTextTextPersonName2)
         btn = findViewById(R.id.button)
+//
+//        if (savedInstanceState!=null){
+//            tv.text = savedInstanceState.getFloat(key,0.0f).toString()
+//        }
 
-        var aa = 0.0
-        var bb =0.0f
-        var cc = 0.0f
+
 
         btn.setOnClickListener {
 
+
             try {
                 aa = et1.text.toString().toDouble()
+                et1.text.clear()
             }catch (e:Exception){
                 Toast.makeText(this,"Write a Double number!",Toast.LENGTH_SHORT).show()
             }
 
             try {
                 bb = et2.text.toString().toFloat()
+                et2.text.clear()
             }catch (e:Exception){
                 Toast.makeText(this,"Write a Float number!",Toast.LENGTH_SHORT).show()
             }
 
-             cc = aa.toFloat() + bb
+             cc = aa.toFloat() * bb
             tv.text = "$cc"
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putFloat(key,tv.text.toString().toFloat())
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        tv.text = savedInstanceState.getFloat(key,0.0f).toString()
+    }
 }
+
